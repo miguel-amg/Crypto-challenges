be69dfd67b0fef26d96ad8cbdc0c899ff5430e05
miguel-amg <a100837@alunos.uminho.pt>
2025-06-25 01:17:51 +0100

    Feat: Added main function

--- set1/challenge2.py
+++ set1/challenge2.py
@@ -17,21 +17,22 @@ def xor (input1: bytes, input2: bytes):
     return result
 
 ########################################################################################################################
+# Main function
+if __name__ == "__main__":
+    # Hexadecimal inputs
+    input1 = "1c0111001f010100061a024b53535009181c"
+    input2 = "686974207468652062756c6c277320657965"
 
-# Hexadecimal inputs
-input1 = "1c0111001f010100061a024b53535009181c"
-input2 = "686974207468652062756c6c277320657965"
+    # Inputs in byte format
+    input1_bytes = bytearray.fromhex(input1)
+    input2_bytes = bytearray.fromhex(input2)
 
-# Inputs in byte format
-input1_bytes = bytearray.fromhex(input1)
-input2_bytes = bytearray.fromhex(input2)
+    # Make the XOR
+    result = xor(input1_bytes, input2_bytes)
 
-# Make the XOR
-result = xor(input1_bytes, input2_bytes)
+    result_bytes = bytes(result)    # ByteArray to bytes
+    result_hex = result_bytes.hex() # Bytes to hex string
 
-result_bytes = bytes(result)    # ByteArray to bytes
-result_hex = result_bytes.hex() # Bytes to hex string
-
-# Verify the result
-print(result_hex)
-assert result_hex == "746865206b696420646f6e277420706c6179"
+    # Verify the result
+    print(result_hex)
+    assert result_hex == "746865206b696420646f6e277420706c6179"
