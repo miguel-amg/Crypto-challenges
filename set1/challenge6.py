c5d04d1d62823d87489f9f9f27da668ab3d61802
miguel-amg <a100837@alunos.uminho.pt>
2025-06-30 02:59:21 +0100

    Feat: challenge 6 almost done

--- set1/challenge6.py
+++ set1/challenge6.py
@@ -5,12 +5,39 @@
 
 # Imports
 import base64
-from challenge4 import brute_force_sbx
 
 # Setup
 input_file_path = "files/input_challenge6.txt"
 
 ########################################################################################################################
+# Frequency of the letters in the english alphabet
+freq = {
+    'a': .08167, 'b': .01492, 'c': .02782, 'd': .04253, 'e': .12702,
+    'f': .02228, 'g': .02015, 'h': .06094, 'i': .06966, 'j': .00153,
+    'k': .00772, 'l': .04025, 'm': .02406, 'n': .06749, 'o': .07507,
+    'p': .01929, 'q': .00095, 'r': .05987, 's': .06327, 't': .09056,
+    'u': .02758, 'v': .00978, 'w': .02360, 'x': .00150, 'y': .01974,
+    'z': .00074, ' ': .13000
+}
+
+# Breaks the single byte cipher
+def single_byte_key(block: bytes) -> int:
+    best_score = float('-inf')
+    best_key = 0
+
+    # Lets check every possible single byte key
+    for k in range(256):
+        xord = bytes(b ^ k for b in block)
+
+        score = sum(freq.get(chr(c).lower(), 0) for c in xord) # Sum the score of the frequency of each char 
+
+        # Save the best key
+        if score > best_score:
+            best_score = score
+            best_key = k
+
+    return best_key
+
 # Calculates the hamming distance between two strings
 def hamming_distance_bytes(input1: bytes, input2: bytes) -> int:
     if len(input1) != len(input2):
@@ -42,13 +69,33 @@ def get_keysizes(input: bytes):
 # Break the repeating-key xor cipher
 def brute_force_rkx(input: bytes):
     keysizes = get_keysizes(input)
-    best_keysize = keysizes[0][0]
+    best_keysize_1 = keysizes[0][0]
+    best_keysize_2 = keysizes[1][0]
+    best_keysize_3 = keysizes[2][0]
+
+    print(brute_force_rkx_specific(input, best_keysize_1))
+    print(brute_force_rkx_specific(input, best_keysize_2))
+    print(brute_force_rkx_specific(input, best_keysize_3))
 
 
-    return best_keysize
+# Breaks the repeating-key xor for a specific key size
+def brute_force_rkx_specific(input: bytes, keysize: int):
+    input_array = bytearray(input)
+    blocks = []
+
+    # Creating blocks where every byte was applied the same char from the key (see steps 5 and 6)
+    for i in range(keysize):
+        block = bytes(input_array[i::keysize]) # Picks in i in key_bytes steps
+        blocks.append(block)
+
+    # For each block we will find the 'single byte xor cipher' key
+    final_key = bytearray()
+    for block in blocks:
+        key_byte = single_byte_key(block)
+        final_key.append(key_byte)
+
+    return final_key.decode('ascii')
     
-
-
 ########################################################################################################################
 
 input_file_bytes = open(input_file_path, "rb").read()
