0d54e021b934a185778066450f0a0f77493f3af9
miguel-amg <a100837@alunos.uminho.pt>
2025-06-30 03:17:12 +0100

    feat: Challenge6 done

--- set1/challenge6.py
+++ set1/challenge6.py
@@ -51,41 +51,47 @@ def hamming_distance_bytes(input1: bytes, input2: bytes) -> int:
 
     return result
 
-# Ranks the possbile keysizes using hamming distance
 def get_keysizes(input: bytes):
-    resulting_keysize = 0
-    resulting_distances = {}
+    resulting = []
 
-    for keysize in range(2,40):
-        value1 = input[0:keysize]            # Get the first keysize number of bytes
-        value2 = input[keysize:(keysize*2)]  # Get the second keysize number of bytes
+    for keysize in range(2, 40):
+        # Extract 4 blocks of 'keysize' size
+        b1 = input[0:keysize]
+        b2 = input[keysize:2*keysize]
+        b3 = input[2*keysize:3*keysize]
+        b4 = input[3*keysize:4*keysize]
+        
+        # If the last block is incomplete we will stop
+        if len(b4) < keysize: break
 
-        distance = hamming_distance_bytes(value1, value2)
+        # Calculate the 6 distances
+        d12 = hamming_distance_bytes(b1, b2)
+        d13 = hamming_distance_bytes(b1, b3)
+        d14 = hamming_distance_bytes(b1, b4)
+        d23 = hamming_distance_bytes(b2, b3)
+        d24 = hamming_distance_bytes(b2, b4)
+        d34 = hamming_distance_bytes(b3, b4)
 
-        resulting_distances[keysize] = distance / keysize  # Normalizing the result by diving by keysize
+        # Get the average of the hamming distances
+        avg_dist = (d12 + d13 + d14 + d23 + d24 + d34) / 6.0
+        norm = avg_dist / keysize
 
-    return sorted(resulting_distances.items(), key=lambda x: x[1]) 
+        resulting.append((keysize, norm))
+
+    # Return the keysizes, ordered by the best hamming distances score
+    return sorted(resulting, key=lambda x: x[1])
 
 # Break the repeating-key xor cipher
 def brute_force_rkx(input: bytes):
     keysizes = get_keysizes(input)
-    best_keysize_1 = keysizes[0][0]
-    best_keysize_2 = keysizes[1][0]
-    best_keysize_3 = keysizes[2][0]
+    best_keysize = keysizes[0][0]
 
-    print(brute_force_rkx_specific(input, best_keysize_1))
-    print(brute_force_rkx_specific(input, best_keysize_2))
-    print(brute_force_rkx_specific(input, best_keysize_3))
-
-
-# Breaks the repeating-key xor for a specific key size
-def brute_force_rkx_specific(input: bytes, keysize: int):
     input_array = bytearray(input)
     blocks = []
 
     # Creating blocks where every byte was applied the same char from the key (see steps 5 and 6)
-    for i in range(keysize):
-        block = bytes(input_array[i::keysize]) # Picks in i in key_bytes steps
+    for i in range(best_keysize):
+        block = bytes(input_array[i::best_keysize]) # Picks in i in key_bytes steps
         blocks.append(block)
 
     # For each block we will find the 'single byte xor cipher' key
@@ -96,8 +102,35 @@ def brute_force_rkx_specific(input: bytes, keysize: int):
 
     return final_key.decode('ascii')
     
-########################################################################################################################
+# Deciphers using 'repeating key xor cipher'
+def rkx_decipher(input: bytes, key: bytes):
+    input_array = bytearray(input)
+    key_array   = bytearray(key)
 
-input_file_bytes = open(input_file_path, "rb").read()
-decrypted_file_bytes = base64.b64decode(input_file_bytes)
-print(brute_force_rkx(decrypted_file_bytes))
+    current_key_byte = key_array[0]
+    current_pos = 0
+
+    # Apply every byte of the key to the input in repeating order
+    for i in range (0, len(input_array)):
+        input_array[i] = input_array[i] ^ current_key_byte
+
+        # Update the current key byte
+        if current_pos == (len(key_array) - 1):
+            current_pos = 0
+        else:
+            current_pos += 1
+        
+        current_key_byte = key_array[current_pos]
+
+    return input_array
+
+
+########################################################################################################################
+# Main function
+if __name__ == "__main__":
+    input_file_bytes = open(input_file_path, "rb").read()
+    decrypted_file_bytes = base64.b64decode(input_file_bytes)
+    key = brute_force_rkx(decrypted_file_bytes)
+    print("Key: " + key)
+    deciphered_file = rkx_decipher(decrypted_file_bytes, bytes(key.encode('ascii')))
+    print(bytes(deciphered_file).decode('ascii'))
